2de7bc24a10b8ddf707a65bf93b08366b85cd246
Pranav Patel <ppranav03@vt.edu>
2026-02-22 12:04:54 -0500

    configured the glare and clicking only one card at a time

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ cards.forEach(card => {
 
     if (card.classList.contains('is_clicked')) return;
 
+    const glare = card.querySelector('.card__glare');
+    if (glare) glare.style.opacity = 1;
+    
     const rect = card.getBoundingClientRect();
 
     // Getting the proportion of distance across a card
@@ -18,8 +21,8 @@ cards.forEach(card => {
     const posy = Math.round(y * 100);
 
     // Tilt degrees (capped at ±15deg)
-    const rotateY = (x - 0.5) * 30;
-    const rotateX = (y - 0.5) * 30;
+    const rotateY = (x - 0.5) * 50;
+    const rotateX = (y - 0.5) * 50;
 
     // Hypotenuse — distance from center, used for brightness
     const hyp = Math.sqrt(
@@ -37,7 +40,10 @@ cards.forEach(card => {
 
   card.addEventListener('mouseleave', () => {
   if (card.classList.contains('is_clicked')) return;
-    // Reset everything smoothly
+
+    const glare = card.querySelector('.card__glare');
+    if (glare) glare.style.opacity = 0;
+
     card.style.transform = 'perspective(600px) rotateX(0deg) rotateY(0deg)';
     card.style.setProperty('--posx', '50%');
     card.style.setProperty('--posy', '50%');
@@ -48,15 +54,29 @@ cards.forEach(card => {
 
 
   card.addEventListener('click', () => {
-    const rect = card.getBoundingClientRect();
-    const centerX = width / 2 - (rect.left + rect.width / 2);
-    const centerY = height / 2 - (rect.top + rect.height / 2);
 
+    const glare = card.querySelector('.card__glare');
+    if (glare) glare.style.opacity = 0;
 
-    if (card.classList.toggle('is_clicked')) {
-      card.style.transform = `translate(${centerX}px, ${centerY}px) scale(2)`;
-    } else {
+    const isAlreadyClicked = card.classList.contains('is_clicked');
+    
+
+    const activeCards = document.querySelectorAll('.card.is_clicked');
+
+    activeCards.forEach(card => {
+      card.classList.remove("is_clicked");
       card.style.transform = '';
+    })
+
+    if (!isAlreadyClicked){
+      const rect = card.getBoundingClientRect();
+      const centerX = width / 2 - (rect.left + rect.width / 2);
+      const centerY = height / 2 - (rect.top + rect.height / 2);
+      card.style.setProperty('--centerX', `${centerX}`)
+      card.style.setProperty('--centerY', `${centerY}`)
+
+      card.classList.toggle("is_clicked");
+      card.style.transform = `translate(${centerX}px, ${centerY}px) scale(2)`;
     }
 
   })
